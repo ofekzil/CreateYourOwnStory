7db27f0d806a31228dbdc62bf45ea5b5a96e5ac2
ofek <ofek@students.cs.ubc.ca>
2022-01-27 17:36:40 -0800

    Added specifications for methods in Prompt and Story classes, and some usage example in main

---
+++ src/main/model/Prompt.java
@@ -0,0 +1,19 @@
+package model;
+
+// represents a prompt for a specific part of a story, to be turned into an answer
+public class Prompt {
+
+    // EFFECTS: constructs a prompt with given string
+    public Prompt(String p) {}
+
+    // MODIFIES: this
+    // EFFECTS: replaces the prompt with the appropriate answer
+    public void changePromptToAnswer(String answer) {}
+
+
+    // getters
+
+    public String getPrompt() {
+        return null; //stub
+    }
+}
---
+++ src/main/model/Story.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.util.List;
+
+// represents a story with a basic skeleton (text not controlled by the user),
+// a list of prompts (the questions and later answers that correspond to the story)
+// and a list of integers representing the locations of each prompt in the story
+public class Story {
+
+    // EFFECTS: constructs a Story with a skeleton, prompts, and locations
+    public Story(List<String> skeleton, List<Prompt> prompts, List<Integer> locations){}
+
+    // REQUIRES: all prompts have been turned into answers
+    // MODIFIES: this
+    // EFFECTS: sets the answers in the appropriate order according to locations
+    public void setPromptsInOrder() {}
+
+    // REQUIRES: all prompts have been turned into answers
+    // AND prompts.size + 1 = skeleton.size() AND no list is empty
+    // EFFECTS: appends story skeleton and prompt answers in appropriate order,
+    // producing the full story
+    public String createStory() {
+        return "";
+    }
+
+    // getters
+
+    public List<String> getSkeleton() {
+        return null; //stub
+    }
+
+    public List<Prompt> getPrompts() {
+        return null; //stub
+    }
+
+    public List<Integer> getLocations() {
+        return null; //stub
+    }
+}
