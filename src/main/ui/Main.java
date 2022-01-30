b78aae2556c2ac64237724190534957c281e9f7c
ofek <ofek@students.cs.ubc.ca>
2022-01-29 16:05:45 -0800

    Added specifications and stubs for StoryApp

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -15,14 +15,14 @@ public class Main {
         prompts.add(p2);
         List<String> skeleton = new ArrayList<String>();
         skeleton.add("One morning ");
-        skeleton.add("got out of bed and went to eat ");
-        skeleton.add("for breakfast.");
+        skeleton.add(" got out of bed and went to eat ");
+        skeleton.add(" for breakfast.");
         List<Integer> locations = new ArrayList<Integer>();
         locations.add(0);
         locations.add(1);
         Story myStory = new Story(skeleton, prompts, locations);
-        p1.changePromptToAnswer(" John Doe");
-        p2.changePromptToAnswer(" eggs");
+        p1.changePromptToAnswer("John Doe");
+        p2.changePromptToAnswer("eggs");
         myStory.setPromptsInOrder();
         String complete = myStory.createStory();
         System.out.println(complete);
---
+++ src/main/ui/StoryApp.java
@@ -0,0 +1,16 @@
+package ui;
+
+import model.Prompt;
+import model.Story;
+
+public class StoryApp {
+
+    // EFFECTS: sets up list of stories and runs the app
+    public StoryApp() {}
+
+    // EFFECTS: chooses story template and male/female story based on user input
+    public void chooseStory() {}
+
+    // EFFECTS: prints the prompts and collects the user's answers, then print the finished story
+    public void makeStory() {}
+}
