c7d9fca432542bf0c2230ebe649779956502331e
ofek <ofek@students.cs.ubc.ca>
2022-01-29 17:08:44 -0800

    Added StoryApp class for ui w/ short sample stories (not final)

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -8,23 +8,6 @@ import model.Prompt;
 
 public class Main {
     public static void main(String[] args) {
-        Prompt p1 = new Prompt("Choose a name");
-        Prompt p2 = new Prompt("Name a food");
-        List<Prompt> prompts = new ArrayList<Prompt>();
-        prompts.add(p1);
-        prompts.add(p2);
-        List<String> skeleton = new ArrayList<String>();
-        skeleton.add("One morning ");
-        skeleton.add(" got out of bed and went to eat ");
-        skeleton.add(" for breakfast.");
-        List<Integer> locations = new ArrayList<Integer>();
-        locations.add(0);
-        locations.add(1);
-        Story myStory = new Story(skeleton, prompts, locations);
-        p1.changePromptToAnswer("John Doe");
-        p2.changePromptToAnswer("eggs");
-        myStory.setPromptsInOrder();
-        String complete = myStory.createStory();
-        System.out.println(complete);
+        new StoryApp();
     }
 }
