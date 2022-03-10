34de0ae1f9565534da24d10c45ab8bd6c5a48df1
ofek <ofek@students.cs.ubc.ca>
2022-03-09 16:09:34 -0800

    Created initial menu and window

--- src/main/ui/StoryAppGUI.java
+++ src/main/ui/StoryAppGUI.java
@@ -9,16 +9,47 @@ public class StoryAppGUI extends JFrame {
     private String[] menuItems = {"New Crazy Morning (male)", "New Crazy Morning (female)", "New In The Kingdom (male)",
             "New In The Kingdom (female)", "Load Story", "Save & Quit"};
     private JTextField input;
+    private JButton submit;
+    private JButton update;
+    private JLabel activePrompt;
+    private JPanel panel;
+    private String chosen;
 
+    // MODIFIES: this
+    // EFFECTS: sets up GUI for initializing app
     public StoryAppGUI() {
         super("Story App");
+        setMinimumSize(new Dimension(800, 600));
         menu = new JComboBox(menuItems);
-        input = new JTextField();
-        //input.setSize(20, 30);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        createPanel();
         add(menu, BorderLayout.NORTH);
-        add(input, BorderLayout.SOUTH);
+        add(panel, BorderLayout.PAGE_END);
+        menu.addActionListener(e -> { // will change to proper item if selected here, so can implement menu choices
+            chosen = (String) menu.getSelectedItem();
+            //activePrompt.setText(chosen);
+        });
         setVisible(true);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates a panel for prompt, answer input, and buttons
+    private void createPanel() {
+        panel = new JPanel();
+        input = new JTextField();
+        input.setPreferredSize(new Dimension(500, 30));
+        Font font = new Font("Arial", Font.PLAIN, 15);
+        input.setFont(font);
+        submit = new JButton("Submit");
+        update = new JButton("Update");
+        activePrompt = new JLabel("Prompt");
+        panel.add(activePrompt);
+        panel.add(Box.createHorizontalStrut(5));
+        panel.add(input);
+        panel.add(Box.createHorizontalStrut(5));
+        panel.add(submit);
+        panel.add(Box.createHorizontalStrut(5));
+        panel.add(update);
     }
 
     public static void main(String[] args) {
